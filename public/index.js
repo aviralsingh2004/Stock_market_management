adad843866eafe7987b719a607fac7e6c6419efe
Yash <148654640+bitluffy@users.noreply.github.com>
2024-12-21 22:34:22 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,7 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    window.location.href = '/home.html'; // Redirect to home.html on login
+});
+
+document.getElementById('signUpButton').addEventListener('click', () => {
+    window.location.href = '/signup.html'; // Redirect to signup.html on register
+});
